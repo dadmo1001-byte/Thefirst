e3c1130ab54ccacf26240867c748b1a7bf89651e
DAD Mo <dadmo1001@gmail.com>
2026-07-29 18:25:51 +0300

    I chadge the file

--- js/master.js
+++ js/master.js
@@ -1,3 +1,10 @@
+// toggle spin class icon 
+document.querySelector(".toggle-setting i").onclick = function () {
+	this.classList.toggle("fa-spin")
+	document.querySelector(".setting-box").classList.toggle("open")
+}
+
+
 // Select Landing Page Element 
 let ladingPage = document.querySelector('.lading-page');
 
@@ -7,7 +14,6 @@ let arrImage = ["01.jpg","02.jpg","03.jpg","04.jpg"];
 // change background-image 
 // get random number 
 setInterval (() => {
-    let randomNumber = Math.floor(Math.random() * arrImage.length);
-    ladingPage.style.backgroundImage = `url("../image/${arrImage[randomNumber]}")` ;
-    console.log(randomNumber);
-}, 5000) 
+	let randomNumber = Math.floor(Math.random() * arrImage.length);
+	ladingPage.style.backgroundImage = `url("../image/${arrImage[randomNumber]}")` ;
+}, 5000);
